9c32d27e10f21fb2827422a1c405e215d3ca153b
Greatnessbio <154548997+Greatnessbio@users.noreply.github.com>
2023-12-21 15:59:32 -0500

    Update 4_PDF-Split.py

--- pages/4_PDF-Split.py
+++ pages/4_PDF-Split.py
@@ -1,6 +1,13 @@
 import PyPDF2
 import streamlit as st
 import io
+import zipfile
+
+# Function to add PDF chunks to a zip file
+def add_to_zip(zip_file, pdf_chunks):
+    for i, chunk in enumerate(pdf_chunks, start=1):
+        chunk.seek(0)
+        zip_file.writestr(f"chunk_{i}.pdf", chunk.read())
 
 # Set the title and description for your Streamlit app
 st.title("PDF Splitter")
@@ -14,54 +21,49 @@ if pdf_file is not None:
     max_chunk_size = st.number_input("Maximum Chunk Size (MB)", min_value=1, value=100)
 
     if st.button("Split PDF"):
+        # Initialize session state for PDF chunks
+        if 'pdf_chunks' not in st.session_state:
+            st.session_state['pdf_chunks'] = []
+
         # Open the uploaded PDF file
         with pdf_file as file:
             reader = PyPDF2.PdfReader(file)
 
-            # Initialize variables to keep track of the current chunk size and the pages in the chunk
+            # Initialize variables for chunking
             current_chunk_size = 0
-            chunk_number = 1
             writer = PyPDF2.PdfWriter()
 
-            # Buffer to store PDF chunks
-            pdf_chunks = []
-
             # Iterate over each page in the PDF
-            for page_num, page in enumerate(reader.pages):
-                # Use in-memory buffer
+            for page in reader.pages:
+                # Use in-memory buffer for temporary storage
                 temp_buffer = io.BytesIO()
                 temp_writer = PyPDF2.PdfWriter()
                 temp_writer.add_page(page)
                 temp_writer.write(temp_buffer)
-
-                # Get the size of the buffer
                 temp_file_size = temp_buffer.tell()
 
-                # Check if adding the current page would exceed the maximum chunk size
+                # Check chunk size
                 if current_chunk_size + temp_file_size > max_chunk_size * 1024 * 1024:
-                    # Save the current chunk to buffer
                     chunk_buffer = io.BytesIO()
                     writer.write(chunk_buffer)
-                    pdf_chunks.append(chunk_buffer)
-
-                    # Reset the variables for the new chunk
+                    st.session_state['pdf_chunks'].append(chunk_buffer.getvalue())
                     current_chunk_size = 0
-                    chunk_number += 1
                     writer = PyPDF2.PdfWriter()
 
-                # Add the current page to the current chunk
                 writer.add_page(page)
                 current_chunk_size += temp_file_size
 
-            # Create the last chunk if there are remaining pages
             if current_chunk_size > 0:
                 chunk_buffer = io.BytesIO()
                 writer.write(chunk_buffer)
-                pdf_chunks.append(chunk_buffer)
+                st.session_state['pdf_chunks'].append(chunk_buffer.getvalue())
 
-            # Provide a download button for the generated chunks
-            st.success("PDF split into smaller chunks.")
-            st.write("Download the split chunks:")
-            for i, chunk in enumerate(pdf_chunks, start=1):
-                chunk.seek(0)
-                st.download_button(f"Download Chunk {i}", chunk, file_name=f"chunk_{i}.pdf", mime="application/octet-stream")
+        st.success("PDF split into smaller chunks.")
+
+    # Create a single download button for all chunks
+    if 'pdf_chunks' in st.session_state and st.session_state['pdf_chunks']:
+        zip_buffer = io.BytesIO()
+        with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+            add_to_zip(zip_file, st.session_state['pdf_chunks'])
+        zip_buffer.seek(0)
+        st.download_button("Download All Chunks", zip_buffer, file_name="pdf_chunks.zip", mime="application/zip")
