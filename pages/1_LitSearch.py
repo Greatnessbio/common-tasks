7cd73577bc9d34134564c17a583fc21932fe0eee
Greatnessbio <154548997+Greatnessbio@users.noreply.github.com>
2023-12-21 22:11:52 -0500

    Update 1_LitSearch.py

--- pages/1_LitSearch.py
+++ pages/1_LitSearch.py
@@ -17,6 +17,10 @@ def format_pubmed_result(title, authors, pub_date_str, journal, doi_link, summar
     formatted_result = f"**[Title: {title}]({link})**\nAuthors: {authors}\nPublished in: {journal} on {pub_date_str}\nDOI: {doi_link}\nSummary: {summary}"
     return formatted_result
 
+def format_google_patents_link(query, link):
+    formatted_result = f"Search for patents related to '{query}' on Google Patents: [View Results]({link})"
+    return formatted_result
+
 def search_biorxiv(query):
     formatted_query = query.replace(' ', '%20')
     url = f"https://www.biorxiv.org/search/{formatted_query}%20jcode%3Abiorxiv%7C%7Cmedrxiv%20numresults%3A50%20sort%3Apublication-date%20direction%3Adescending%20format_result%3Astandard"
@@ -30,7 +34,7 @@ def search_biorxiv(query):
         authors = article.find('span', class_='highwire-citation-authors').get_text().strip() if article.find('span', class_='highwire-citation-authors') else 'No authors listed'
         pub_date_str = article.find('span', class_='highwire-cite-metadata-date').get_text().strip() if article.find('span', class_='highwire-cite-metadata-date') else None
         pub_date = parse_date(pub_date_str)
-        journal = 'bioRxiv'  # Since the source is bioRxiv
+        journal = 'bioRxiv'
         doi_link = "https://doi.org/" + article.find('span', class_='highwire-cite-metadata-doi').get_text().strip() if article.find('span', class_='highwire-cite-metadata-doi') else None
         link = "https://www.biorxiv.org" + article.find('a', class_='highwire-cite-linked-title')['href']
         results.append((title, authors, pub_date_str, pub_date, summary, journal, doi_link, link))
@@ -55,6 +59,10 @@ def search_pubmed(query):
         results.append((title, authors, pub_date_str, pub_date, summary, journal, doi_link, link))
     return results
 
+def google_patents_url(query):
+    formatted_query = query.replace(' ', '+')
+    return f"https://patents.google.com/?q=({formatted_query})&num=25&scholar&oq={formatted_query}&sort=new"
+
 st.title("Literature Search App")
 
 query = st.text_input("Enter your search query:")
@@ -74,5 +82,11 @@ if st.button("Search"):
             for title, authors, pub_date_str, _, summary, journal, doi_link, link in pubmed_results:
                 formatted_result = format_pubmed_result(title, authors, pub_date_str, journal, doi_link, summary, link)
                 st.markdown(formatted_result)
+
+        with st.spinner("Preparing Google Patents link..."):
+            patents_link = google_patents_url(query)
+            st.subheader("Google Patents Results")
+            formatted_patents_link = format_google_patents_link(query, patents_link)
+            st.markdown(formatted_patents_link)
     else:
         st.warning("Please enter a search query.")
