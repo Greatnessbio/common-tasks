ebe80777b5adb9709d3f77dea24efa9be0a9f386
Greatnessbio <154548997+Greatnessbio@users.noreply.github.com>
2023-12-21 21:51:33 -0500

    Update 1_LitSearch.py

--- pages/1_LitSearch.py
+++ pages/1_LitSearch.py
@@ -5,12 +5,13 @@ from datetime import datetime
 
 def parse_date(date_string):
     try:
-        return datetime.strptime(date_string, '%Y-%m-%d') if date_string is not None else None
+        return datetime.strptime(date_string, '%d %b %Y') if date_string is not None else None
     except ValueError:
         return None
 
 def search_biorxiv(query):
-    url = f"https://www.biorxiv.org/search/{query.replace(' ', '+')}"
+    formatted_query = query.replace(' ', '%20')
+    url = f"https://www.biorxiv.org/search/{formatted_query}%20jcode%3Abiorxiv%7C%7Cmedrxiv%20numresults%3A50%20sort%3Apublication-date%20direction%3Adescending%20format_result%3Astandard"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     articles = soup.find_all('li', class_='search-result')
@@ -25,12 +26,11 @@ def search_biorxiv(query):
         doi_link = "https://doi.org/" + article.find('span', class_='highwire-cite-metadata-doi').get_text().strip() if article.find('span', class_='highwire-cite-metadata-doi') else None
         link = "https://www.biorxiv.org" + article.find('a', class_='highwire-cite-linked-title')['href']
         results.append((title, authors, pub_date_str, pub_date, summary, journal, doi_link, link))
-
-    # Sort by publication date, placing None values at the end
-    return sorted(results, key=lambda x: x[3] if x[3] is not None else datetime.min, reverse=True)
+    return results
 
 def search_pubmed(query):
-    url = f"https://pubmed.ncbi.nlm.nih.gov/?term={query.replace(' ', '+')}"
+    formatted_query = query.replace(' ', '+')
+    url = f"https://pubmed.ncbi.nlm.nih.gov/?term={formatted_query}&sort=date&size=20"
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     articles = soup.find_all('article', class_='full-docsum')
@@ -39,15 +39,13 @@ def search_pubmed(query):
         title = article.find('a', class_='docsum-title').get_text().strip()
         summary = article.find('div', class_='full-view-snippet').get_text().strip() if article.find('div', class_='full-view-snippet') else 'No summary available'
         authors = article.find('span', class_='docsum-authors').get_text().strip() if article.find('span', class_='docsum-authors') else 'No authors listed'
-        pub_date_str = article.find('span', class_='docsum-journal-citation').get_text().strip().split('.')[0] if article.find('span', class_='docsum-journal-citation') else None
+        pub_date_str = article.find('span', class='docsum-journal-citation').get_text().strip().split('.')[0] if article.find('span', class='docsum-journal-citation') else None
         pub_date = parse_date(pub_date_str)
-        journal = article.find('span', class_='docsum-journal-citation').get_text().strip().split('.')[1].strip() if article.find('span', class_='docsum-journal-citation') else None
-        doi_link = f"https://doi.org/{article.find('span', class_='docsum-doi').get_text().strip()}" if article.find('span', class_='docsum-doi') else None
-        link = "https://pubmed.ncbi.nlm.nih.gov" + article.find('a', class_='docsum-title')['href']
+        journal = article.find('span', class='docsum-journal-citation').get_text().strip().split('.')[1].strip() if article.find('span', class='docsum-journal-citation') else None
+        doi_link = f"https://doi.org/{article.find('span', class='docsum-doi').get_text().strip()}" if article.find('span', class='docsum-doi') else None
+        link = "https://pubmed.ncbi.nlm.nih.gov" + article.find('a', class='docsum-title')['href']
         results.append((title, authors, pub_date_str, pub_date, summary, journal, doi_link, link))
-
-    # Sort by publication date, placing None values at the end
-    return sorted(results, key=lambda x: x[3] if x[3] is not None else datetime.min, reverse=True)
+    return results
 
 st.title("Literature Search App")
 
