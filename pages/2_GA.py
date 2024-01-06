55df27e6b168b14353700aec5159cb4c2bc9f7b2
Greatnessbio <154548997+Greatnessbio@users.noreply.github.com>
2024-01-05 19:17:14 -0500

    Update 2_GA.py

--- pages/2_GA.py
+++ pages/2_GA.py
@@ -3,6 +3,8 @@ import streamlit as st
 from io import StringIO
 
 def read_section(lines):
+  if not lines:
+    return pd.DataFrame()  # Return an empty DataFrame if section_lines is empty
   return pd.read_csv(StringIO(''.join(lines)), skipinitialspace=True)
 
 # Upload the CSV file
