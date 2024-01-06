c1cfa1b4d1cd72f6db2c5412082d5378e32e279b
Greatnessbio <154548997+Greatnessbio@users.noreply.github.com>
2024-01-05 19:16:07 -0500

    Update 2_GA.py

--- pages/2_GA.py
+++ pages/2_GA.py
@@ -3,32 +3,32 @@ import streamlit as st
 from io import StringIO
 
 def read_section(lines):
-    return pd.read_csv(StringIO(''.join(lines)), skipinitialspace=True)
+  return pd.read_csv(StringIO(''.join(lines)), skipinitialspace=True)
 
 # Upload the CSV file
 uploaded_file = st.file_uploader("Upload your CSV file", type="csv")
 
 if uploaded_file is not None:
-    # Read the entire CSV file
-    file_content = uploaded_file.readlines()
+  # Read the entire CSV file
+  file_content = uploaded_file.getvalue().decode().splitlines()
 
-    # Create a dictionary of DataFrames
-    dfs = {}
-    i = 0
-    while i < len(file_content):
-        line = file_content[i].decode()
-        if line.startswith('Nth day') or line.startswith('First user default channel group') or line.startswith('Session default channel group') or line.startswith('Session Google Ads campaign') or line.startswith('Cohort'):
-            section_lines = []
-            i += 1
-            while i < len(file_content) and file_content[i].decode().strip() != '':
-                section_lines.append(file_content[i].decode())
-                i += 1
-            df = read_section(section_lines)
-            dfs[line.strip()] = df
-        else:
-            i += 1
+  # Create a dictionary of DataFrames
+  dfs = {}
+  i = 0
+  while i < len(file_content):
+    line = file_content[i]
+    if line.startswith('Nth day') or line.startswith('First user default channel group') or line.startswith('Session default channel group') or line.startswith('Session Google Ads campaign') or line.startswith('Cohort'):
+      section_lines = []
+      i += 1
+      while i < len(file_content) and file_content[i].strip() != '':
+        section_lines.append(file_content[i])
+        i += 1
+      df = read_section(section_lines)
+      dfs[line.strip()] = df
+    else:
+      i += 1
 
-    # Display the DataFrames
-    for header, df in dfs.items():
-        st.write(f"DataFrame for {header}:")
-        st.dataframe(df)
+  # Display the DataFrames
+  for header, df in dfs.items():
+    st.write(f"DataFrame for {header}:")
+    st.dataframe(df)
