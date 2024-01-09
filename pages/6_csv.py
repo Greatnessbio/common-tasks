449a14d381b02285503a8f1e3801d15b33f13b29
Greatnessbio <154548997+Greatnessbio@users.noreply.github.com>
2024-01-08 21:34:07 -0500

    Update 6_csv.py

--- pages/6_csv.py
+++ pages/6_csv.py
@@ -6,12 +6,12 @@ def process_file(uploaded_file):
     file_extension = os.path.splitext(uploaded_file.name)[1]
 
     if file_extension.lower() == '.xlsx':
-        xls = pd.ExcelFile(uploaded_file)
-        sheet_names = xls.sheet_names
+        xls = pd.read_excel(uploaded_file, sheet_name=None)
+        sheet_names = list(xls.keys())
     elif file_extension.lower() == '.xls':
         # For '.xls' files, use an appropriate engine like 'xlrd' (version 1.2.0 or earlier)
-        xls = pd.read_excel(uploaded_file, engine='xlrd') # Ensure you have the compatible version of xlrd
-        sheet_names = xls.sheet_names
+        xls = pd.read_excel(uploaded_file, sheet_name=None, engine='xlrd') # Ensure you have the compatible version of xlrd
+        sheet_names = list(xls.keys())
     else:
         st.error("Invalid file format. Please upload a valid Excel file.")
         return
@@ -19,8 +19,7 @@ def process_file(uploaded_file):
     csv_folder = 'csv_files'
     os.makedirs(csv_folder, exist_ok=True)
 
-    for sheet_name in sheet_names:
-        df = xls.parse(sheet_name)
+    for sheet_name, df in xls.items():
         csv_filename = f"{csv_folder}/{sheet_name}.csv"
         df.to_csv(csv_filename, index=False)
 
